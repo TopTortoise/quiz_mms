e463346cbe8426a3c831568901c3f7c49b5ad8ad
TopTortoise <paul.b0o3721@gmail.com>
2021-06-10 21:02:25 +0200

    deleted Food file, added QuizPage

---
+++ src/QuizPage.java
@@ -0,0 +1,20 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+
+public class QuizPage extends Page{
+
+    protected QuizPage(JButton[] buttons, String title, String text) {
+        super(buttons, title, text);
+        //TODO Auto-generated constructor stub
+    }
+
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
--- src/quiz.java
+++ src/quiz.java
@@ -1,4 +1,3 @@
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
@@ -14,13 +13,27 @@ class quiz{
     public static void main(String[] args) {
         
         JButton[] h = new JButton[4];
-        for (int i = 0; i < h.length; i++) {
+        for (int i = 0; i < h.length-1; i++) {
             h[i] = new JButton("hello "+i);
+        }
+        try {
+            h[3] = new Picture("pic/VEHICLES/tram.jpg");
+        } catch (IOException e2) {
+            // TODO Auto-generated catch block
+            e2.printStackTrace();
         }
         Page p = new Page(h, "Mine", "HELLO" ){
 
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                if(e.getSource().equals(h[3]))
+                    try {
+                        ((Picture) h[3]).pixi();
+                    } catch (IOException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
                 // TODO Auto-generated method stub
                 
             }
@@ -28,17 +41,12 @@ class quiz{
         };
         
       //  p.setButtonVisible(true);
-        try {
-            BufferedImage b = ImageIO.read(new File("pic/CITIES/athen.jpg"));
-            p.panel.add(new JLabel(new ImageIcon(b)));
-        } catch (IOException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-        
+   
+        //this will always need to be at the end
         p.add(p.panel);
         p.pack();
-        p.setSize(1000, 1000);
+        //
+        p.setSize(2000, 2000);
 
     }
 
