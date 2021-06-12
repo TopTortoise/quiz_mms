77d0e5344b9efa7733d1b466235157819b52c0de
TopTortoise <paul.b0o3721@gmail.com>
2021-06-12 20:23:01 +0200

    Picture.java clean up

--- src/Picture.java
+++ src/Picture.java
@@ -1,6 +1,5 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
